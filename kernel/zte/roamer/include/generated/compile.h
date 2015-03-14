db47439c9696ab5de42b484b4654045a7d7ceeb6
M <m@i5.(none)>
2015-03-14 18:55:40 -0500

    add kernel source

---
+++ kernel/zte/roamer/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 6 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#6 PREEMPT Thu Aug 28 05:01:24 CDT 2014"
+#define LINUX_COMPILE_TIME "05:01:24"
+#define LINUX_COMPILE_BY "m"
+#define LINUX_COMPILE_HOST "i5"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
